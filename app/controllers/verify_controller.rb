ed0c99f91821c0ef128e3c76275485458230e3a6
Ray Lin <raytlin@gmail.com>
2015-06-07 09:23:52 -0700

    seems to be working for all casesgit add . git add .

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -5,7 +5,7 @@ class VerifyController < ApplicationController
       @url = params[:url]
       url = URI.parse(@url)
       res = Net::HTTP.get_response(url)
-
+      #get if redirect happened
       if res['location']
           newURI = URI.parse(res['location'])
       else
@@ -13,8 +13,13 @@ class VerifyController < ApplicationController
       end
       
       fixedURI = URI.parse(newURI.scheme + "://" + newURI.host + "/favicon.ico")
-      stringURL = newURI.scheme + "://" + newURI.host + "/favicon.ico"
+      
       response = Net::HTTP.get_response(fixedURI)
+      
+      #get if second redirect happened
+      if response['location']
+          response = Net::HTTP.get_response(URI.parse(response['location']))
+      end
       
       send_data response.body, :type => 'image/png',:disposition => 'inline'
 
